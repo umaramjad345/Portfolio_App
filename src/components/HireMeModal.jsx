fec56676d7b32aa3c9349986f4760dd32c433761
umaramjad345 <umarziaii345@gmail.com>
2024-05-08 00:26:16 +0500

    adding changes

--- src/components/HireMeModal.jsx
+++ src/components/HireMeModal.jsx
@@ -2,12 +2,7 @@ import { motion } from "framer-motion";
 import { FiX } from "react-icons/fi";
 import { FloatingLabel, Select, Textarea } from "flowbite-react";
 
-const selectOptions = [
-  "Web Application",
-  "Mobile Application",
-  "UI/UX Design",
-  "Branding",
-];
+const selectOptions = ["Web Application", "Mobile Application", "UI/UX Design"];
 
 const HireMeModal = ({ onClose, onRequest }) => {
   return (
